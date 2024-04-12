97a31dca160989f4b0fdbc70abde509492b0c744
satyam saxena <satyams584@gmail.com>
2024-04-12 18:37:36 +0530

    127: create project, create post entity class and configure application properties

---
+++ src/main/java/com/myBlog/entity/Post.java
@@ -0,0 +1,23 @@
+package com.myBlog.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="post")
+@Data
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
+
